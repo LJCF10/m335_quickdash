4cee43ea907b670fe45dc9ff57b2495605913f3d
lenny cruzinha <cruzinha.lenny@gmail.com>
2021-02-02 15:09:37 +0100

    Added Camera Activity to MainActivity

--- QuickDash/app/src/main/java/ch/zli/quickdash/activities/MainActivity.java
+++ QuickDash/app/src/main/java/ch/zli/quickdash/activities/MainActivity.java
@@ -43,5 +43,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            image.setImageBitmap(imageBitmap);
+        }
+    }
+
 
 }
